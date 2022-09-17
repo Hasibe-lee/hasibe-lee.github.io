035d7ba0f568cbdeaea4762e74849ee6ec7ad240
Hasibe-lee <sdhicb@126.com>
2022-09-17 20:29:52 +0800

    完成了部分关于与个人的开发

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 window.onload = function () {
+  let navbar = document.querySelector('.navbar')
   let photoSelf = document.querySelector('.photo-self')
   let photo = photoSelf.querySelector('img')
   let desc = document.querySelector('.desc')
+  let floor = document.querySelector('.floor')
+  let oneSelf = floor.querySelector('.photo-self')
   let op = 0
 
   photo.timer = setInterval(() => {
@@ -13,4 +16,28 @@ window.onload = function () {
     desc.style.opacity = op
   }, 100)
 
+  // 节流函数
+  let throttle = function (fn, delay) {
+    let timer = null
+    return function () {
+      if (!timer) {
+        let arg = arguments
+        timer = setTimeout(() => {
+          fn.apply(this, arg)
+          timer = null
+        }, delay)
+      }
+    }
+  }
+
+  window.onscroll = throttle(function () {
+    if (scrollY > 0) {
+      navbar.classList.add('sticky')
+    } else {
+      navbar.classList.remove('sticky')
+    }
+    if (scrollY >= 400) {
+      oneSelf.style.animation = 'rotateImg 2.5s linear forwards'
+    }
+  }, 60)
 }
